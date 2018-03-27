b51bd91f9c6072d41d9e74d34f93dba1dd3a03f5
Gawron Michel <michel.gawron@saint-gobain.com>
2018-03-27 11:34:14 +0200

    Drawing subway in the main class

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import load_data as ld
 
 
 h, w, names, coordinates, distances = ld.get_data_to_dict(ld.load_file("./metro.gr"))
+ld.draw_subway(750, 750, coordinates, distances)
 p, d = ld.dijkstra(distances, 0, 350)
 print(p)
 print(list(map(lambda x: names[x], p)))
